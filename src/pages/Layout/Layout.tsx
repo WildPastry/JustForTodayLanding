4abeb895159fb2595a5b6317cbf44466ababfb68
WildPastry <mparkernzdev@gmail.com>
2024-03-19 20:07:25 +1300

    Add store logos

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -2,6 +2,8 @@ import Footer from '../../components/Footer';
 import Reflections from '../../components/Reflections';
 import Steps from '../../components/Steps';
 import Traditions from '../../components/Traditions';
+import appleStoreBadge from './../../assets/img/apple.svg';
+import googeStoreBadge from './../../assets/img/google.svg';
 import jftLogo from '../../assets/just-for-today.svg';
 import styles from './Layout.module.css';
 
@@ -14,6 +16,16 @@ const Layout: React.FC = (): JSX.Element => {
           Just for <span className={styles['logo-text-colour']}>today</span>
         </h1>
       </div>
+      <img
+        className={styles['store-badge']}
+        src={appleStoreBadge}
+        alt='Apple Store Badge'
+      />
+      <img
+        className={styles['store-badge']}
+        src={googeStoreBadge}
+        alt='Googe Store Badge'
+      />
       <Reflections />
       <Steps />
       <Traditions />
