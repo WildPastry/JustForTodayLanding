961be66450edb812a4bd844a7de8c2a71414671f
WildPastry <mparkernzdev@gmail.com>
2024-03-21 08:41:35 +1300

    Bump versions. Add grid.

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -12,6 +12,12 @@ const Layout: React.FC = (): JSX.Element => {
       <Reflections />
       <Steps />
       <Logo />
+      <div className={`grid grid-cols-6 gap-4`}>
+        <div className={`${styles.bg} col-start-2 col-span-4`}>01</div>
+        <div className={`${styles.bg} col-start-1 col-end-3`}>02</div>
+        <div className={`${styles.bg} col-end-7 col-span-2`}>03</div>
+        <div className={`${styles.bg} col-start-1 col-end-7`}>04</div>
+      </div>
       <Traditions />
       <Footer />
       <Badges />
