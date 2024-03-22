90a1ccd9fa148fc1e61b2a14ead9bc2a2479641f
WildPastry <mparkernzdev@gmail.com>
2024-03-22 18:38:15 +1300

    Updating text

--- src/components/Steps.tsx
+++ src/components/Steps.tsx
@@ -3,9 +3,9 @@ const Steps: React.FC = (): JSX.Element => {
     <section>
       <h2 className='text-3xl'>Steps</h2>
       <p>
-        AA’s Twelve Steps are a set of spiritual principles. When practiced as a
-        way of life, they can expel the obsession to drink and enable the
-        sufferer to recover from alcoholism.
+        A set of spiritual principles. When practiced as a way of life, they can
+        expel the obsession to drink and enable the sufferer to recover from
+        alcoholism.
       </p>
     </section>
   );
