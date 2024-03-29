ba068b5939489103e2fcc4738aa845a8c4b7c3b1
WildPastry <mparkernzdev@gmail.com>
2024-03-29 17:42:27 +1300

    Adjust styling

--- src/pages/Privacy.tsx
+++ src/pages/Privacy.tsx
@@ -10,57 +10,54 @@ const Privacy: React.FC = (): JSX.Element => {
         use, and safeguard your data when you use our mobile application.
       </p>
       <hr className='h-px my-10 bg-gray-600 border-0' />
-      <div>
-        <div className='mb-10'>
-          <h3 className='text-lg text-spotBlue mb-2'>Data Collection</h3>
-          <p className='text-md'>
-            We want to reassure our users that JustForToday does not collect any
-            personal data or information from you while you use our mobile
-            application. We do not gather any data about your device, usage
-            patterns, or any other information that could potentially identify
-            you.
-          </p>
-        </div>
-        <div className='mb-10'>
-          <h3 className='text-lg text-spotBlue mb-2'>Data Usage</h3>
-          <p className='text-md'>
-            Since we do not collect any data, we do not use your personal
-            information for any purpose.
-          </p>
-        </div>
-        <div className='mb-10'>
-          <h3 className='text-lg text-spotBlue mb-2'>Data Sharing</h3>
-          <p className='text-md'>
-            We do not share any data, personal or otherwise, with third parties.
-            Your information is not disclosed, sold, or rented to any external
-            entities.
-          </p>
-        </div>
-        <div className='mb-10'>
-          <h3 className='text-lg text-spotBlue mb-2'>Changes</h3>
-          <p className='text-md'>
-            We reserve the right to update this Privacy Statement from time to
-            time. Any changes will be reflected on this page.
-          </p>
-        </div>
-        <div className='mb-10'>
-          <h3 className='text-lg text-spotBlue mb-2'>Contact Us</h3>
-          <p className='text-md mb-2'>
-            If you have any questions or concerns regarding this Privacy
-            Statement or the privacy practices of JustForToday, please contact
-            us
-            <a className='ml-1' href='mailto:justfortoday@mikeparker.co.nz'>
-              here
-            </a>
-            .
-          </p>
-          <p className='text-md'>
-            By using JustForToday, you signify your acceptance of this Privacy
-            Statement. If you do not agree to this Privacy Statement, please
-            refrain from using our mobile application. Thank you for trusting
-            JustForToday with your privacy.
-          </p>
-        </div>
+      <div className='mb-10'>
+        <h3 className='text-lg text-spotBlue mb-2'>Data Collection</h3>
+        <p className='text-md'>
+          We want to reassure our users that JustForToday does not collect any
+          personal data or information from you while you use our mobile
+          application. We do not gather any data about your device, usage
+          patterns, or any other information that could potentially identify
+          you.
+        </p>
+      </div>
+      <div className='mb-10'>
+        <h3 className='text-lg text-spotBlue mb-2'>Data Usage</h3>
+        <p className='text-md'>
+          Since we do not collect any data, we do not use your personal
+          information for any purpose.
+        </p>
+      </div>
+      <div className='mb-10'>
+        <h3 className='text-lg text-spotBlue mb-2'>Data Sharing</h3>
+        <p className='text-md'>
+          We do not share any data, personal or otherwise, with third parties.
+          Your information is not disclosed, sold, or rented to any external
+          entities.
+        </p>
+      </div>
+      <div className='mb-10'>
+        <h3 className='text-lg text-spotBlue mb-2'>Changes</h3>
+        <p className='text-md'>
+          We reserve the right to update this Privacy Statement from time to
+          time. Any changes will be reflected on this page.
+        </p>
+      </div>
+      <div className='mb-10'>
+        <h3 className='text-lg text-spotBlue mb-2'>Contact Us</h3>
+        <p className='text-md mb-2'>
+          If you have any questions or concerns regarding this Privacy Statement
+          or the privacy practices of JustForToday, please contact us
+          <a className='ml-1' href='mailto:justfortoday@mikeparker.co.nz'>
+            here
+          </a>
+          .
+        </p>
+        <p className='text-md'>
+          By using JustForToday, you signify your acceptance of this Privacy
+          Statement. If you do not agree to this Privacy Statement, please
+          refrain from using our mobile application. Thank you for trusting
+          JustForToday with your privacy.
+        </p>
       </div>
     </section>
   );
--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -2,21 +2,19 @@ const Support: React.FC = (): JSX.Element => {
   return (
     <section className='w-700 m-auto'>
       <div className='my-10'>
-        <h2 className='text-2xl text-center'>FAQ</h2>
+        <h2 className='text-2xl text-center'>Support</h2>
       </div>
       <hr className='h-px my-10 bg-gray-600 border-0' />
-      {/* <div className='w-700 m-auto'>
-        <div className='mb-10'>
-          <h3 className='text-lg text-spotBlue mb-2'>Data Collection</h3>
-          <p className='text-md'>
-            We want to reassure our users that JustForToday does not collect any
-            personal data or information from you while you use our mobile
-            application. We do not gather any data about your device, usage
-            patterns, or any other information that could potentially identify
-            you.
-          </p>
-        </div>
-      </div> */}
+      <div className='mb-10'>
+        <h3 className='text-lg text-spotBlue mb-2'>Contact</h3>
+        <p className='text-xl leading-8 mb-2'>
+          For any support or questions please contact the team
+          <a className='ml-1' href='mailto:justfortoday@mikeparker.co.nz'>
+            here
+          </a>
+          .
+        </p>
+      </div>
     </section>
   );
 };
