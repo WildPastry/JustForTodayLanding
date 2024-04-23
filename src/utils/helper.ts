3c21428498cb97bdfa144e08664f15015fd8a3cd
WildPastry <mparkernzdev@gmail.com>
2024-04-24 07:21:45 +1200

    Adjust helper

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,8 +1,13 @@
-const mediaMatch = window.matchMedia('(min-width: 460px)');
+const screenWidth = window.innerWidth;
 
 export const selectWidth = (id: string): string => {
- if (id === 'reflections') {
-  return mediaMatch ? '204' : '300'
- }
- return '300'
+  if (id === 'reflections') {
+    return screenWidth < 460 ? '204' : '204';
+  } else if (id === 'steps') {
+    return screenWidth < 460 ? '93' : '74';
+  } else if (id === 'traditions') {
+    return screenWidth < 460 ? '186' : '186';
+  } else {
+    return '0';
+  }
 };
