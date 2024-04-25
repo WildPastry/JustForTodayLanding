aa05bb43f09f7b7481309c74f76bf5bd171aedae
WildPastry <mparkernzdev@gmail.com>
2024-04-25 16:30:56 +1200

    Update screen width

--- src/components/Traditions.tsx
+++ src/components/Traditions.tsx
@@ -1,9 +1,13 @@
+import React from 'react';
 import Rectangle from './Rectangle';
+import { useScreenWidth } from '../utils/useScreenWidth';
 
 const Traditions: React.FC = (): JSX.Element => {
+  const screenWidth = useScreenWidth(400); // Set your desired breakpoint here
+
   return (
     <section className='relative'>
-      <Rectangle colour='midBlue' width='186' />
+      <Rectangle colour='midBlue' width={screenWidth.toString()} />
       <h1 className='text-3xl mb-5 z-10 relative mobileSmall:text-2xl'>Traditions</h1>
       <p className='text-md'>
         Providing guidelines for relationships between the groups, members, the
--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -3,11 +3,11 @@ import { HiWrenchScrewdriver } from 'react-icons/hi2';
 const Support: React.FC = (): JSX.Element => {
   return (
     <section className='w-700 m-auto max783:w-auto'>
-      <div className='mb-10'>
+      <div className='mb-5 min674:my-10'>
         <h2 className='text-2xl text-center'>Support</h2>
       </div>
       <HiWrenchScrewdriver className='text-gray-900 m-auto mb-10 mobileSmall:text-200 min460:text-300' />
-      <p className='text-xl leading-8 text-center'>
+      <p className='text:md min674:text-xl leading-8 text-center'>
         For any support or questions please contact the team
         <a className='ml-1' href='mailto:justfortoday@mikeparker.co.nz'>
           here
