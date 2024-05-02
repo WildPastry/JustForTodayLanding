e10e4c8a4687c7a4cba6cffb51aa5b263206008f
WildPastry <mparkernzdev@gmail.com>
2024-05-02 17:00:07 +1200

    Add apple store badge back in

--- src/components/Badges.tsx
+++ src/components/Badges.tsx
@@ -1,18 +1,18 @@
-// import appleStoreBadge from './../assets/img/apple.svg';
+import appleStoreBadge from './../assets/img/apple.svg';
 import googeStoreBadge from './../assets/img/google.svg';
 
 const Badges: React.FC = (): JSX.Element => {
   return (
     <section className='max460:flex max460:flex-col'>
-      {/* <div className='align-bottom inline-block me-10 max460:mb-5 max460:m-auto'>
+      <div className='align-bottom inline-block me-10 max460:mb-5 max460:m-auto'>
         <a
           className='align-bottom inline-block'
-          href='https://www.apple.com/nz/app-store/'
+          href='https://apps.apple.com/us/app/justfortoday/id6479473282'
           target='_blank'
           rel='noopener noreferrer'>
           <img className='h-12 min460:h-16' src={appleStoreBadge} alt='Apple Store Badge' />
         </a>
-      </div> */}
+      </div>
       <div className='align-bottom inline-block max460:m-auto'>
         <a
           className='align-bottom inline-block'
